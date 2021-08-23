607a55003f3e83c5334be351724b757d66c68156
EduCCor <ecordovc@everis.com>
2021-08-23 15:16:15 -0500

    services debits

---
+++ springboot-services-debits/src/main/java/com/everis/springboot/debits/SpringbootServicesDebitsApplication.java
@@ -0,0 +1,13 @@
+package com.everis.springboot.debits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicesDebitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicesDebitsApplication.class, args);
+	}
+
+}
